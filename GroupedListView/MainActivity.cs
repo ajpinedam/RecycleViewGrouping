b632aebb9969597e25311aff32a8bffcbe2c5eaf
Andres Pineda <pineda.andres@gmail.com>
2019-04-20 21:04:06 -0400

    Remove unnecesary code for the example

--- GroupedListView/MainActivity.cs
+++ GroupedListView/MainActivity.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
-using Android.Views;
-using Android.Widget;
 
 namespace GroupedListView
 {
@@ -24,10 +19,8 @@ namespace GroupedListView
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
 
             _recyclerView = FindViewById<RecyclerView>(Resource.Id.rv_my_data);
 
@@ -38,8 +31,6 @@ namespace GroupedListView
 
             _recyclerView.SetLayoutManager(_layoutManager);
             _recyclerView.SetAdapter(_dataAdapter);
-
-            fab.Click += FabOnClick;
         }
 
         private IList<MyData> GetMyData()
@@ -60,37 +51,6 @@ namespace GroupedListView
             };
 
             return data;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
-            {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
